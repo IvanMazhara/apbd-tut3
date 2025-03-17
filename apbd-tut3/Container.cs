fddbaafe62cc92bcd202a58900455fef89763e72
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-17 17:50:27 +0100

    Initial commit

---
+++ apbd-tut3/Container.cs
@@ -0,0 +1,11 @@
+﻿namespace apbd_tut3;
+
+public class Container
+{
+    public int cargo_mass { get; set; } //Mass of the cargo (what's inside of container)
+    public int container_weight { get; set; } //Mass of the container itself
+    public double height { get; set; }
+    public double depth { get; set; }
+    public string serial_number { get; set; }
+    public int max_payload { get; set; }
+}
