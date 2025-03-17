12c3e4d6d87c919524aec2da7968d0a4ad0ff1de
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-17 18:39:30 +0100

    Container class improvements, IHazardNotifier interface added

--- apbd-tut3/Container.cs
+++ apbd-tut3/Container.cs
@@ -2,25 +2,25 @@
 
 public class Container
 {
-    Random random = new Random();
-    public ContainerType type { get; set; }
-    public int cargo_mass { get; set; } //Mass of the cargo (what's inside of container)
-    public int container_weight { get; set; } //Mass of the container itself
-    public double height { get; set; }
-    public double depth { get; set; }
-    public string serial_number { get; set; }
-    public int max_payload { get; set; }
+    Random Random = new Random();
+    public ContainerType Type { get; set; }
+    public int CargoMass { get; set; } //Mass of the cargo (what's inside of container)
+    public int ContainerWeight { get; set; } //Mass of the container itself
+    public double Height { get; set; }
+    public double Depth { get; set; }
+    public string SerialNumber { get; set; }
+    public int MaxPayload { get; set; }
 
-    Container(int cargo_mass, int container_weight, ContainerType type, double height, double depth, int max_payload)
+    public Container(int CargoMass, int ContainerWeight, ContainerType Type, double Height, double Depth, int MaxPayload)
     {
-        this.cargo_mass = cargo_mass;
-        this.container_weight = container_weight;
-        this.type = type;
-        this.height = height;
-        this.depth = container_weight;
-        this.serial_number = "KON-" + type + "-" + random.Next(1, 9999);
-        this.max_payload = max_payload;
+        this.CargoMass = CargoMass;
+        this.ContainerWeight = ContainerWeight;
+        this.Type = Type;
+        this.Height = Height;
+        this.Depth = Depth;
+        SerialNumber = "KON-" + Type + "-" + Random.Next(1, 9999);
+        this.MaxPayload = MaxPayload;
         
-        if(this.cargo_mass > this.container_weight) throw new Exception($"The cargo is way too heavy for this container");
+        if(this.CargoMass + this.ContainerWeight > this.MaxPayload) throw new Exception($"The cargo is way too heavy for this container");
     }
 }
--- apbd-tut3/Program.cs
+++ apbd-tut3/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using apbd_tut3;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Container container = new Container(10, 450, ContainerType.Refrigerator, 200.0, 50.0, 500);
+        Console.WriteLine(container.SerialNumber);
+    }
+}
