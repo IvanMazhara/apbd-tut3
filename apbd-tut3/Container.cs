00111d41b570df854c0b1d147bebbef89b599f74
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-17 18:00:49 +0100

    Container's constructor created

--- apbd-tut3/Container.cs
+++ apbd-tut3/Container.cs
@@ -2,10 +2,22 @@
 
 public class Container
 {
+    public ContainerType type { get; set; }
     public int cargo_mass { get; set; } //Mass of the cargo (what's inside of container)
     public int container_weight { get; set; } //Mass of the container itself
     public double height { get; set; }
     public double depth { get; set; }
     public string serial_number { get; set; }
     public int max_payload { get; set; }
+
+    Container(int cargo_mass, int container_weight, ContainerType type, double height, double depth, int max_payload)
+    {
+        this.cargo_mass = cargo_mass;
+        this.container_weight = container_weight;
+        this.type = type;
+        this.height = height;
+        this.depth = container_weight;
+        this.serial_number = "KON-" + type;
+        this.max_payload = max_payload;
+    }
 }
