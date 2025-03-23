1fb596ac765b45b26a0a6fd2f8a210e537ee4bb4
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-23 19:01:49 +0100

    Ship class added

---
+++ apbd-tut3/Ship.cs
@@ -0,0 +1,27 @@
+﻿namespace apbd_tut3;
+
+public class Ship
+{
+    public List<Container> Containers { get; set; }
+    public double MaxSpeed { get; set; } 
+    public int MaxContainers { get; set; }
+    public double MaxWeight { get; set; }
+    
+    public Ship(double maxSpeed, int maxContainers, double maxWeight)
+    {
+        Containers = new List<Container>();
+        MaxSpeed = maxSpeed;
+        MaxContainers = maxContainers;
+        MaxWeight = maxWeight;
+    }
+
+    public void LoadContainer(Container container)
+    {
+        Containers.Add(container);
+    }
+
+    public void LoadContainers(List<Container> containers)
+    {
+        Containers.AddRange(containers);
+    }
+}
