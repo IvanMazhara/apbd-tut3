631660deccd1bd983ad72b1c788dc5863aaba481
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-23 20:19:26 +0100

    Update commit

--- apbd-tut3/obj/Debug/net8.0/apbd-tut3.AssemblyInfo.cs
+++ apbd-tut3/obj/Debug/net8.0/apbd-tut3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c060be16306f08f77fb2e745a69434245a368d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a604ac6f480aa840ba5583392300783d22716024")]
 [assembly: System.Reflection.AssemblyProductAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
