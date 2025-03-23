e377cca85c142aad8f7660a7895d6bebbb2cbd3b
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-23 12:43:32 +0100

    Directory Container created

--- apbd-tut3/obj/Debug/net8.0/apbd-tut3.AssemblyInfo.cs
+++ apbd-tut3/obj/Debug/net8.0/apbd-tut3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c8f86f52701e13f31bec95f43db413426b3a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apbd-tut3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
